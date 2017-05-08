5b1589b661da02659cd8aa81bf9a33b550cc2852
gmyou <gmyou71@gmail.com>
2017-05-08 11:23:07 +0900

    unicode

--- regEx.js
+++ regEx.js
@@ -33,3 +33,16 @@ stickyFlag = regex.test(str); // false (lastIndex is taken into account with sti
 console.log(stickyFlag);
 stickyFlag = regex.lastIndex; // 0 (reset after match failure)
 console.log(stickyFlag);
+
+var text = 'Образец text на русском языке';
+var regex = /[\u0400-\u04FF]+/g;
+
+var match = regex.exec(text);
+console.log(match[0]);        // logs 'Образец'
+console.log(regex.lastIndex); // logs '7'
+
+var match2 = regex.exec(text);
+console.log(match2[0]);       // logs 'на' [did not log 'text']
+console.log(regex.lastIndex); // logs '15'
+
+// and so on
