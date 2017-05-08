2b1289de0d183fd66a0d20acd42978a033a074b2
gmyou <gmyou71@gmail.com>
2017-05-08 11:19:10 +0900

    split line

--- regEx.js
+++ regEx.js
@@ -3,4 +3,13 @@ var str = 'John Smith';
 var newstr = str.replace(re, '$2, $1');
 console.log(newstr);
 
+var text = 'Some text\nAnd some more\r\nAnd yet\rThis is the end';
+var lines = text.split(/\r\n|\r|\n/);
+console.log(lines); // logs [ 'Some text', 'And some more', 'And yet', 'This is the end' ]
 
+var text = `Some text
+And some more
+And yet
+This is the end`;
+var lines = text.split(/\r\n|\r|\n/);
+console.log(lines); // logs [ 'Some text', 'And some more', 'And yet', 'This is the end' ]
