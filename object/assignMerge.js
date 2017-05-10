5c4d499e0491564969cc31e6291cc2be24a62ec2
Jack You <gmyou71@gmail.com>
2017-05-10 18:54:09 +0900

    sync

--- object/assignMerge.js
+++ object/assignMerge.js
@@ -4,3 +4,25 @@ let mergeObj = Object.assign(oneObj, twoObj);
 console.log(oneObj);
 console.log(twoObj);
 console.log(mergeObj);
+
+console.log(Object.is(oneObj, mergeObj));
+
+// 연동
+mergeObj.one = 3;
+console.log(oneObj);
+console.log(mergeObj);
+
+console.log(Object.is(oneObj, mergeObj));
+
+
+var o1 = { a: 1 };
+var o2 = { b: 2 };
+var o3 = { c: 3 };
+
+var obj = Object.assign(o1, o2, o3);
+console.log(obj); // { a: 1, b: 2, c: 3 }
+console.log(o1);  // { a: 1, b: 2, c: 3 }, 타겟 오브젝트, 그 자체도 변화합니다
+
+o1.a = 4;
+console.log(obj);
+console.log(o1);
