face0c939218a547288210a5eb6f4c719d89a097
gmyou <gmyou71@gmail.com>
2017-05-14 12:31:06 +0900

    polyfill

--- string/stringSubStr.js
+++ string/stringSubStr.js
@@ -8,3 +8,24 @@ console.log('(-20, 2): ' + str.substr(-20, 2)); // '(-20, 2): ab'
 console.log('(-20): '    + str.substr(-20));    // '(-20): abcdefghij'
 console.log('(20, 2): '  + str.substr(20, 2));  // '(20, 2): '
 console.log('(20, -2): '  + str.substr(20, -2));  // '(20, -2): '
+
+// only run when the substr() function is broken
+console.log('ab'.substr(-1));
+if ('ab'.substr(-1) != 'b') {
+  /**
+   *  Get the substring of a string
+   *  @param  {integer}  start   where to start the substring
+   *  @param  {integer}  length  how many characters to return
+   *  @return {string}
+   */
+  String.prototype.substr = function(substr) {
+    return function(start, length) {
+      // call the original method
+      return substr.call(this,
+      	// did we get a negative start, calculate how much it is from the beginning of the string
+        // adjust the start parameter for negative value
+        start < 0 ? this.length + start : start,
+        length)
+    }
+  }(String.prototype.substr);
+}
