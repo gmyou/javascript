f1dfc300af320c6593ab343677da3e098b2d630e
gmyou <gmyou71@gmail.com>
2017-05-14 12:55:12 +0900

    identity

---
+++ equality.js
@@ -0,0 +1,18 @@
+var num = 0;
+var obj = new String("0");
+var str = "0";
+var b = false;
+
+console.log(num === num); // true
+console.log(obj === obj); // true
+console.log(str === str); // true
+
+console.log(num === obj); // false
+console.log(num === str); // false
+console.log(obj === str); // false
+console.log(null === undefined); // false
+console.log(obj === null); // false
+console.log(obj === undefined); // false
+
+b = num;
+console.log(b === num); // true
