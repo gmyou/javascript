7be740f64386d9d8a0c63a18ab279a463ea5b03d
gmyou <gmyou71@gmail.com>
2017-05-14 12:59:24 +0900

    loose

--- equality.js
+++ equality.js
@@ -16,3 +16,22 @@ console.log(obj === undefined); // false
 
 b = num;
 console.log(b === num); // true
+
+
+var num = 0;
+var obj = new String("0");
+var str = "0";
+var b = false;
+
+console.log(num == num); // true
+console.log(obj == obj); // true
+console.log(str == str); // true
+
+console.log(num == obj); // true
+console.log(num == str); // true
+console.log(obj == str); // true
+console.log(null == undefined); // true
+
+// 둘 다 false, 드문 경우를 제외하고는
+console.log(obj == null);
+console.log(obj == undefined);
