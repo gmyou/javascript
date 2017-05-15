2e56efdd6035e918b03ef0f7787c9643dcdfce5d
Jack You <gmyou71@gmail.com>
2017-05-15 16:19:47 +0900

    multi param

--- template/taggedTemplate.js
+++ template/taggedTemplate.js
@@ -7,3 +7,38 @@ function taggedFunction(text, value) {
     console.log("3:", value);
 }
 taggedFunction `1+2=${one + two}`;
+
+
+var a = 5;
+var b = 10;
+
+function tag(strings, ...values) {
+  console.log(strings[0]); // "Hello "
+  console.log(strings[1]); // " world "
+  console.log(strings[2]); // ""
+  console.log(values[0]);  // 15
+  console.log(values[1]);  // 50
+
+  return "Bazinga!";
+}
+
+tag`Hello ${ a + b } world ${ a * b }`;
+// "Bazinga!"
+
+
+function template(strings, ...keys) {
+  return (function(...values) {
+    var dict = values[values.length - 1] || {};
+    var result = [strings[0]];
+    keys.forEach(function(key, i) {
+      var value = Number.isInteger(key) ? values[key] : dict[key];
+      result.push(value, strings[i + 1]);
+    });
+    return result.join('');
+  });
+}
+
+var t1Closure = template`${0}${1}${0}!`;
+t1Closure('Y', 'A');  // "YAY!" 
+var t2Closure = template`${0} ${'foo'}!`;
+t2Closure('Hello', {foo: 'World'});  // "Hello World!"
