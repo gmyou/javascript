6908c6054f62775b89ad054b78f9581e352a1ac3
Jack You <gmyou71@gmail.com>
2017-05-16 17:02:43 +0900

    classes

--- class/this.js
+++ class/this.js
@@ -8,3 +8,14 @@ class Sports {
 };
 Sports.setGround('상암구장');
 console.log(Sports.getGround());
+
+class Sports2 {
+    constructor() {
+        console.log(Sports2.getGround());
+        console.log(this.constructor.getGround());
+    }
+    static getGround() {
+        return '고양구장';
+    }
+};
+let obj = new Sports2();
