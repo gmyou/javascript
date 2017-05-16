f086d80aa309f0703f88bbbdf25c444552b55897
Jack You <gmyou71@gmail.com>
2017-05-16 14:11:49 +0900

    generator

--- generator/declation.js
+++ generator/declation.js
@@ -6,3 +6,80 @@ console.log( typeof sports );
 
 let getObj = sports(1, 2);
 console.log( typeof getObj );
+
+// 반복기
+function makeIterator(array){
+    var nextIndex = 0;
+
+    return {
+       next: function(){
+           return nextIndex < array.length ?
+               {value: array[nextIndex++], done: false} :
+               {done: true};
+       }
+    }
+}
+
+var it = makeIterator(['yo', 'ya']);
+console.log(it.next().value); // 'yo'
+console.log(it.next().value); // 'ya'
+console.log(it.next().done);  // true
+
+
+// iterable
+for(let value of ["a", "b", "c"]){
+    console.log(value)
+}
+// "a"
+// "b"
+// "c"
+
+[..."abc"] // ["a", "b", "c"]
+
+function* gen(){
+  yield* ["a", "b", "c"]
+}
+
+gen().next() // { value:"a", done:false }
+
+
+// 생성기
+function* idMaker(){
+  var index = 0;
+  while(true)
+    yield index++;
+}
+
+var gen2 = idMaker();
+
+console.log(gen2.next().value); // 0
+console.log(gen2.next().value); // 0
+console.log(gen2.next().value); // 0
+
+function* fibonacci(){
+  var fn1 = 0;
+  var fn2 = 1;
+  while (true){
+    var current = fn1;
+    fn1 = fn2;
+    fn2 = current + fn1;
+    var reset = yield current;
+    if (reset){
+        fn1 = 0;
+        fn2 = 1;
+    }
+  }
+}
+
+var sequence = fibonacci();
+console.log(sequence.next().value);     // 0
+console.log(sequence.next().value);     // 1
+console.log(sequence.next().value);     // 1
+console.log(sequence.next().value);     // 2
+console.log(sequence.next().value);     // 3
+console.log(sequence.next().value);     // 5
+console.log(sequence.next().value);     // 8
+console.log(sequence.next(true).value); // 0
+console.log(sequence.next().value);     // 1
+console.log(sequence.next().value);     // 1
+console.log(sequence.next().value);     // 2
