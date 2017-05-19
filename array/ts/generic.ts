68fb2b7a97d74ea76a8901aeeadea69c52e684f4
Jack You <gmyou71@gmail.com>
2017-05-19 17:12:59 +0900

    typescript init

---
+++ array/ts/generic.ts
@@ -0,0 +1,6 @@
+let fruits:Array<string> = ["banana","apple","mango"];
+let fruits2:Array<string> = [];
+fruits2.push("banana");
+fruits2.push("apple");
+fruits2.push("mango");
+console.log(fruits, fruits2);
